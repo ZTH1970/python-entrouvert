eda5ddda8bf11cbfa4546b2e74ca6eabb66739b6
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-05-13 15:22:59 +0200

    djommon: add a django VersionMiddleware

---
+++ entrouvert/djommon/middleware.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+import json
+
+from entrouvert.wsgi import middleware
+
+'''Version middleware to retrieves Entr'ouvert packages versions'''
+
+class VersionMiddleware:
+    def process_request(self, request):
+        if request.method == 'GET' and request.path == '/__version__':
+            packages_version = middleware.VersionMiddleware.get_packages_version()
+            return HttpResponse(json.dumps(packages_version),
+                    content_type='text/json')
+        return None
