1bcca4496d0d190a4f0d38ae7a5e168c3d436b3f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-01-21 21:41:59 +0100

    humantime: adapt datetime timezone before formating the first time

--- entrouvert/djommon/humantime/utils.py
+++ entrouvert/djommon/humantime/utils.py
@@ -7,12 +7,13 @@ from django.template.defaultfilters import date
 def datetime2human(dt, include_time=False, days_limit=7):
     '''Format a datetime object for human consumption'''
     if isinstance(dt, datetime.datetime):
+        dt = localtime(dt)
         time = dt.strftime('%H:%M')
     else:
         dt = datetime.datetime(year=dt.year, month=dt.month, day=dt.day,
                 tzinfo=get_default_timezone())
+        dt = localtime(dt)
         include_time = False
-    dt = localtime(dt)
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
     subdate = dt.date()
